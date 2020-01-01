19e8833a1986aa01341f78c9b0883953429d4f25
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2020-01-01 19:00:08 +0100

    two application

---
+++ vtk/DicomViewer/DicomViewer.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.uic import loadUiType
+
+import vtk
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+
+ui, QMainWindow = loadUiType('DicomViewer.ui')
+
+class DicomVis(QMainWindow, ui):
+
+  def __init__(self, parent = None):
+    self.reader = vtk.vtkDICOMImageReader()
+    self.dataExtent = []
+    self.dataDimensions = []
+    self.dataRange = ()
+
+    # initialize GUI
+    super(DicomVis,self).__init__()
+    self.setupUi(self)
+
+    # Replace widgets
+    parent = self.XYPlaneWidget.parentWidget()
+    tmp = QVTKRenderWindowInteractor(self)
+    parent.layout().replaceWidget(self.XYPlaneWidget, tmp)
+    self.XYPlaneWidget = tmp
+
+    parent = self.YZPlaneWidget.parentWidget()
+    tmp = QVTKRenderWindowInteractor(self)
+    parent.layout().replaceWidget(self.YZPlaneWidget, tmp)
+    self.YZPlaneWidget = tmp
+
+    parent = self.XZPlaneWidget.parentWidget()
+    tmp = QVTKRenderWindowInteractor(self)
+    parent.layout().replaceWidget(self.XZPlaneWidget, tmp)
+    self.XZPlaneWidget = tmp
+
+    parent = self.VolumeWidget.parentWidget()
+    tmp = QVTKRenderWindowInteractor(self)
+    parent.layout().replaceWidget(self.VolumeWidget, tmp)
+    self.VolumeWidget = tmp
+
+    self.WindowCenterSlider.setRange(0, 1000)
+    self.WindowWidthSlider.setRange(0, 1000)
+
+    # define viewers
+    [self.viewerXY, self.viewerYZ, self.viewerXZ] = [vtk.vtkImageViewer2() for x in range(3)]
+
+    # attach interactors to viewers
+    self.viewerXY.SetupInteractor(self.XYPlaneWidget)
+    self.viewerYZ.SetupInteractor(self.YZPlaneWidget)
+    self.viewerXZ.SetupInteractor(self.XZPlaneWidget)
+
+    # set render windows for viewers
+    self.viewerXY.SetRenderWindow(self.XYPlaneWidget.GetRenderWindow())
+    self.viewerYZ.SetRenderWindow(self.YZPlaneWidget.GetRenderWindow())
+    self.viewerXZ.SetRenderWindow(self.XZPlaneWidget.GetRenderWindow())
+
+    # set slicing orientation for viewers
+    self.viewerXY.SetSliceOrientationToXZ()
+    self.viewerYZ.SetSliceOrientationToYZ()
+    self.viewerXZ.SetSliceOrientationToXY()
+
+    # rotate image
+    act = self.viewerYZ.GetImageActor()
+    act.SetOrientation(90, 0, 0)
+
+    # setup volume rendering
+    self.volRender = vtk.vtkRenderer()
+    self.volRenWin = self.VolumeWidget.GetRenderWindow()
+    self.volRenWin.AddRenderer(self.volRender)
+
+    self.volumeMapper = vtk.vtkFixedPointVolumeRayCastMapper()
+    #self.volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
+
+    volumeColor = vtk.vtkColorTransferFunction()
+    volumeColor.AddRGBPoint(0,    0.0, 0.0, 0.0)
+    volumeColor.AddRGBPoint(500,  1.0, 0.5, 0.3)
+    volumeColor.AddRGBPoint(1000, 1.0, 0.5, 0.3)
+    volumeColor.AddRGBPoint(1150, 1.0, 1.0, 0.9)
+    self.volumeColor = volumeColor
+
+    volumeScalarOpacity = vtk.vtkPiecewiseFunction()
+    volumeScalarOpacity.AddPoint(0,    0.00)
+    volumeScalarOpacity.AddPoint(50,  0.15)
+    volumeScalarOpacity.AddPoint(100, 0.15)
+    volumeScalarOpacity.AddPoint(115, 0.85)
+    self.volumeScalarOpacity = volumeScalarOpacity
+
+    volumeGradientOpacity = vtk.vtkPiecewiseFunction()
+    volumeGradientOpacity.AddPoint(0,   0.0)
+    volumeGradientOpacity.AddPoint(100,  0.5)
+    volumeGradientOpacity.AddPoint(500, 1)
+    self.volumeGradientOpacity = volumeGradientOpacity
+
+    volumeProperty = vtk.vtkVolumeProperty()
+    volumeProperty.SetColor(volumeColor)
+    volumeProperty.SetScalarOpacity(volumeScalarOpacity)
+    volumeProperty.SetGradientOpacity(volumeGradientOpacity)
+    volumeProperty.SetInterpolationTypeToLinear()
+    volumeProperty.ShadeOn()
+    volumeProperty.SetAmbient(0.4)
+    volumeProperty.SetDiffuse(0.6)
+    volumeProperty.SetSpecular(0.2)
+    self.volumeProperty = volumeProperty
+
+    volume = vtk.vtkVolume()
+    volume.SetMapper(self.volumeMapper)
+    volume.SetProperty(self.volumeProperty)
+    self.volume = volume
+
+    self.volRender.AddViewProp(volume)
+
+  def updateData(self, studydata):
+    self.load_study_from_path(studydata.getPath())
+
+  def load_study_from_path(self, studyPath):
+    # Update reader
+    self.reader.SetDirectoryName(studyPath)
+    self.reader.Update()
+
+    self.xyMapper = vtk.vtk
+
+    # Get data dimensionality
+    self.dataExtent = self.reader.GetDataExtent()
+    dataDimensionX = self.dataExtent[1]-self.dataExtent[0]
+    dataDimensionY = self.dataExtent[3]-self.dataExtent[2]
+    dataDimensionZ = self.dataExtent[5]-self.dataExtent[4]
+    self.dataDimensions = [dataDimensionX, dataDimensionY, dataDimensionZ]
+
+    # Calculate index of middle slice
+    midslice1 = int((self.dataExtent[1]+self.dataExtent[0])//2)
+    midslice2 = int((self.dataExtent[3]+self.dataExtent[2])//2)
+    midslice3 = int((self.dataExtent[5]+self.dataExtent[4])//2)
+
+    # Calculate enter
+    center = [midslice1, midslice2, midslice3]
+
+    # Get data range
+    self.dataRange = self.reader.GetOutput().GetPointData().GetArray("DICOMImage").GetRange()
+
+    # Set current slice to the middle one
+    for pair in zip([self.viewerXY, self.viewerYZ, self.viewerXZ], [midslice1, midslice2, midslice3]):
+      pair[0].SetInputData(self.reader.GetOutput())
+      pair[0].SetSlice(pair[1])
+      pair[0].Render()
+    pass
+
+    # Set range and proper value for slice sliders
+    for pair in zip([self.XYSlider, self.YZSlider, self.XZSlider,], self.dataDimensions, [midslice1, midslice2, midslice3]):
+      pair[0].setRange(0, pair[1])
+      pair[0].setValue(pair[2])
+
+    # Set range and value for windowing sliders
+    self.WindowCenterSlider.setRange(int(self.dataRange[0]), int(self.dataRange[1]))
+    self.WindowCenterSlider.setValue(int(self.dataRange[0] + self.dataRange[1])//2)
+    self.WindowWidthSlider.setRange(1, int(self.dataRange[1]))
+    self.WindowWidthSlider.setValue((1+int(self.dataRange[1]))//2)
+
+    # set input for volume renderer
+    self.volumeMapper.SetInputConnection(self.reader.GetOutputPort())
+    self.volRenWin.Render()
+
+  # setup slots for slicing sliders
+  @QtCore.pyqtSlot(int)
+  def on_XYSlider_valueChanged(self, value):
+    self.viewerXY.SetSlice(value)
+
+  @QtCore.pyqtSlot(int)
+  def on_YZSlider_valueChanged(self, value):
+    self.viewerYZ.SetSlice(value)
+
+  @QtCore.pyqtSlot(int)
+  def on_XZSlider_valueChanged(self, value):
+    self.viewerXZ.SetSlice(value)
+
+  # Setup slots for windowing sliders
+  @QtCore.pyqtSlot(int)
+  def on_WindowCenterSlider_valueChanged(self, value):
+    for x in [self.viewerXY, self.viewerXZ, self.viewerYZ]:
+      x.SetColorLevel(value)
+      x.Render()
+
+  @QtCore.pyqtSlot(int)
+  def on_WindowWidthSlider_valueChanged(self, value):
+    for x in [self.viewerXY, self.viewerXZ, self.viewerYZ]:
+      x.SetColorWindow(value)
+      x.Render()
+
+if __name__ == "__main__":
+  import sys
+  app = QtWidgets.QApplication(sys.argv)
+  window = DicomVis()
+  window.show()
+
+  studyPath = "/home/jmh/github/ITKexample/data/CTbrain/"
+  window.load_study_from_path(studyPath)
+  sys.exit(app.exec_())
+
+# Local variables: #
+# tab-width: 2 #
+# python-indent: 2 #
+# indent-tabs-mode: nil #
+# End: #
---
+++ vtk/Mini_app/Mini_app.py
@@ -0,0 +1,119 @@
+import sys
+import numpy as np
+
+import vtk
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+from PyQt5 import (QtWidgets, QtGui, uic)
+from PyQt5.QtCore import QObject
+
+ui, QMainWindow = uic.loadUiType('Mini_app_Qt_VTK.ui')
+class ViewersApp(QMainWindow, ui):
+  def __init__(self):
+    #Parent constructor
+    super(ViewersApp, self).__init__()
+    self.vtk_widget = None
+    self.setup()
+
+  def setup(self):
+    self.setupUi(self)
+    self.vtk_widget = QMeshViewer(self.vtk_panel)
+
+    # add a layout to let the vtk panel grow/shrink with window resize
+    self.vtk_layout = QtWidgets.QHBoxLayout()
+    self.vtk_layout.addWidget(self.vtk_widget)
+    self.vtk_layout.setContentsMargins(0,0,0,0)
+    self.vtk_panel.setLayout(self.vtk_layout)
+
+    # connects slots and signals for all 3 widgets
+    self.comboBox.activated.connect(self.vtk_widget.Switch_Mode)
+    self.Resolution.valueChanged.connect(self.vtk_widget.set_Resolution)
+    self.radioButton.clicked.connect(self.vtk_widget.button_event)
+
+  def initialize(self):
+    self.vtk_widget.start()
+
+class QMeshViewer(QtWidgets.QFrame):
+  def __init__(self, parent):
+    super(QMeshViewer,self).__init__(parent)
+
+    # Make the actual QtWidget a child so that it can be re_parented
+    interactor = QVTKRenderWindowInteractor(self)
+    self.layout = QtWidgets.QHBoxLayout()
+    self.layout.addWidget(interactor)
+    self.layout.setContentsMargins(0,0,0,0)
+    self.setLayout(self.layout)
+
+    # set up my VTK Visualization pipeline
+    # cut-and-paste from https://lorensen.github.io/VTKExamples/site/Python/GeometricObjects/Sphere/
+    colors = vtk.vtkNamedColors()
+
+    # Create a sphere
+    sphereSource = vtk.vtkSphereSource()
+    sphereSource.SetCenter(0.0,0.0,0.0)
+    sphereSource.SetRadius(5.0)
+    # Make the surface smooth
+    sphereSource.SetPhiResolution(100)
+    sphereSource.SetThetaResolution(100)
+
+    # Create a mapper
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputConnection(sphereSource.GetOutputPort())
+
+    # Create an actor
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    actor.GetProperty().SetColor(colors.GetColor3d("Cornsilk"))
+    actor.GetProperty().SetRepresentation(0)
+    actor.GetProperty().SetDiffuseColor(colors.GetColor3d("Cornsilk"))
+    actor.GetProperty().SetDiffuse(0.8)
+    actor.GetProperty().SetSpecular(0.5)
+    actor.GetProperty().SetSpecularColor(1.,1.,1.)
+    actor.GetProperty().SetSpecularPower(30.)
+
+    renderer =  vtk.vtkOpenGLRenderer()
+    render_window = interactor.GetRenderWindow()
+    render_window.AddRenderer(renderer)
+
+    # TEST
+    # interactor.SetRenderWindow(render_window)
+    #render_window.SetInteractor(interactor)
+
+    renderer.AddActor(actor)
+    renderer.SetBackground(colors.GetColor3d("DarkGreen"))
+
+    self.render_window = render_window
+    self.interactor = interactor
+    self.renderer = renderer
+    self.sphere = sphereSource
+    self.actor = actor
+
+  def start(self):
+    self.interactor.Initialize()
+    # If a big Qt application call app.exec instead
+    self.interactor.Start()
+
+  def Switch_Mode(self, new_value):
+    self.actor.GetProperty().SetRepresentation(new_value)
+    self.render_window.Render()
+
+  def button_event(self, new_value):
+    if new_value:
+      print("Button was clicked")
+
+  def set_Resolution(self, new_value):
+    self.sphere.SetPhiResolution(new_value)
+    self.sphere.SetThetaResolution(new_value)
+    self.render_window.Render()
+
+if __name__ == '__main__':
+  app = QtWidgets.QApplication(["Mini-App"])
+  main_window = ViewersApp()
+  main_window.show()
+  main_window.initialize()
+  app.exec_()
+
+# Local variables: #
+# tab-width: 2 #
+# python-indent: 2 #
+# indent-tabs-mode: nil #
+# End: #
