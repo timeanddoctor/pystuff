35a6c4e544636cfd19eec650ec12ddd8abaf1809
Hansen <jem@bkmedical.com>
2021-01-21 23:18:03 +0100

    more progress

--- vtk/FourPaneViewer.py
+++ vtk/FourPaneViewer.py
@@ -2,10 +2,10 @@
 import os
 import sys
 
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtCore
 from PyQt5.uic import loadUiType
 from PyQt5.QtCore import QCoreApplication, Qt
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QSplitter, QAction, QFileDialog, QApplication, QFrame
 
 import vtk
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
@@ -17,77 +17,161 @@ ui, QMainWindow = loadUiType(ui_file)
 
 class FourPaneViewer(QMainWindow, ui):
   def __init__(self):
-    #Parent constructor
     super(FourPaneViewer, self).__init__()
     self.vtk_widget = None
     self.setup()
+
+  def onLoadClicked(self):
+    options = QFileDialog.Options()
+    options |= QFileDialog.DontUseNativeDialog
+    fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;MHD Files (*.mhd)", options=options)
+    if fileName:
+      self.loadFile(fileName)
+  def loadFile(self, fileName):
+    # Load meta file
+    print("loading: %s" % fileName)
+    reader = vtk.vtkMetaImageReader()
+    reader.SetFileName(fileName)
+    reader.Update()
+    return self.loadFile1(reader)
+  def loadFile1(self, reader):
+    imageDims = reader.GetOutput().GetDimensions()
+    for i in range(3):
+      self.vtk_widgets[i].viewer.GetInteractor().EnableRenderOff()
+    self.vtk_widgets[3].viewer.GetInteractor().EnableRenderOff()
+
+    # Turn-off plane widgets
+    for i in range(3):
+      self.planeWidget[i].Off()
+
+    for i in range(3):
+      print("%d" % i)
   def setup(self):
     self.setupUi(self)
 
-    if 0:
-      self.vtk_widget = TestMe(self.vtk_panel)
-      
-      # add a layout to let the vtk panel grow/shrink with window resize
-      self.vtk_layout = QtWidgets.QHBoxLayout()
-      self.vtk_layout.addWidget(self.vtk_widget)
-      self.vtk_layout.setContentsMargins(0,0,0,0)
-      self.vtk_panel.setLayout(self.vtk_layout)
-    else:
-      self.vtk_widgets = [TestMe(self.vtk_panel),
-                          TestMe(self.vtk_panel),
-                          TestMe(self.vtk_panel),
-                          TestMe(self.vtk_panel)]
-      self.horz_layout0 = QHBoxLayout()
-      # Vertical spliiter
-      self.vert_splitter = QtWidgets.QSplitter(Qt.Vertical)
-      # Horizontal splitter 0
-      self.horz_splitter0 = QtWidgets.QSplitter(Qt.Horizontal)
-      self.horz_splitter0.addWidget(self.vtk_widgets[0])
-      self.horz_splitter0.addWidget(self.vtk_widgets[1])
-      self.vert_splitter.addWidget(self.horz_splitter0)
-      # Horizontal splitter 1
-      self.horz_splitter1 = QtWidgets.QSplitter(Qt.Horizontal)
-      self.horz_splitter1.addWidget(self.vtk_widgets[2])
-      self.horz_splitter1.addWidget(self.vtk_widgets[3])
-      self.vert_splitter.addWidget(self.horz_splitter1)
+    loadAct = QAction('&Open', self)
+    loadAct.setShortcut('Ctrl+O')
+    loadAct.setStatusTip('Load data')
+    loadAct.triggered.connect(self.onLoadClicked)
 
-      # Add vertical spliiter to out layout
-      self.horz_layout0.addWidget(self.vert_splitter)
-      self.horz_layout0.setContentsMargins(0,0,0,0)
+    menubar = self.menuBar()
+    fileMenu = menubar.addMenu('&File')
+    fileMenu.addAction(loadAct)    
 
-      # Set layout of frame
-      self.vtk_panel.setLayout(self.horz_layout0)
+    self.vtk_widgets = [TestMe(self.vtk_panel,0),
+                        TestMe(self.vtk_panel,1),
+                        TestMe(self.vtk_panel,2),
+                        TestMe(self.vtk_panel)]
 
-    # TODO: Add splitters, 
+    # Make all views share the same cursor object
+    for i in range(3):
+      self.vtk_widgets[i].SetResliceCursor(self.vtk_widgets[0].GetResliceCursor())
+
+    # Make 3D viewer
+    picker = vtk.vtkCellPicker()
+    picker.SetTolerance(0.005)
+
+    ipwProp = vtk.vtkProperty()
+    ren = vtk.vtkRenderer()
+
+    interactor = QVTKRenderWindowInteractor()
+    renderWindow = vtk.vtkGenericOpenGLRenderWindow()
+    interactor.SetRenderWindow(renderWindow)
+
+    interactor.GetRenderWindow().AddRenderer(ren)
     
+    self.planeWidget = []
+    for i in range(3):
+      pw = vtk.vtkImagePlaneWidget()
+      pw.SetInteractor(interactor)
+      pw.SetPicker(picker)
+      pw.RestrictPlaneToVolumeOn()
+      color = [0.0,0.0,0.0]
+      color[i] = 1
+      pw.GetPlaneProperty().SetColor(color)
+      for j in range(3):
+        color[j] = color[j] / 4.0
+      self.vtk_widgets[i].viewer.GetRenderer().SetBackground(color)
+
+      pw.SetTexturePlaneProperty(ipwProp)
+      pw.TextureInterpolateOff()
+      pw.SetResliceInterpolateToLinear()
+      pw.DisplayTextOn()
+      pw.SetDefaultRenderer(ren)
+      self.planeWidget.append(pw)
+
+    # Establish callbacks (TODO)
+    
+
+    
+    self.vtk_widgets[0].show()
+    self.vtk_widgets[1].show()
+    self.vtk_widgets[2].show()
+
+    for i in range(3):
+      self.vtk_widgets[i].viewer.GetImageActor().SetVisibility(False)
+      
+    self.horz_layout0 = QHBoxLayout()
+    # Vertical spliiter
+    self.vert_splitter = QSplitter(Qt.Vertical)
+    # Horizontal splitter 0
+    self.horz_splitter0 = QSplitter(Qt.Horizontal)
+    self.horz_splitter0.addWidget(self.vtk_widgets[0])
+    self.horz_splitter0.addWidget(self.vtk_widgets[1])
+    self.vert_splitter.addWidget(self.horz_splitter0)
+    # Horizontal splitter 1
+    self.horz_splitter1 = QSplitter(Qt.Horizontal)
+    self.horz_splitter1.addWidget(self.vtk_widgets[2])
+    self.horz_splitter1.addWidget(self.vtk_widgets[3])
+    self.vert_splitter.addWidget(self.horz_splitter1)
+
+    # Add vertical spliiter to out layout
+    self.horz_layout0.addWidget(self.vert_splitter)
+    self.horz_layout0.setContentsMargins(0,0,0,0)
+
+    # Set layout of frame
+    self.vtk_panel.setLayout(self.horz_layout0)
+
   def initialize(self):
     print("initialize")
     #self.vtk_widget.start()
     self.vtk_widgets[0].start()
     self.vtk_widgets[1].start()
-
-
     
-class Viewer2D(QtWidgets.QFrame):
-  def __init__(self, parent):
+class Viewer2D(QFrame):
+  def __init__(self, parent, iDim=0):
     super(Viewer2D, self).__init__(parent)
     interactor = QVTKRenderWindowInteractor(self)
-    self.layout = QtWidgets.QHBoxLayout(self)
+    self.layout = QHBoxLayout(self)
     self.layout.addWidget(interactor)
     self.layout.setContentsMargins(0,0,0,0)
+    self.setLayout(self.layout)
 
-    self.viewer = vtk.vtkImageViewer2()
+    self.viewer = vtk.vtkResliceImageViewer()
     self.viewer.SetupInteractor(interactor)
     self.viewer.SetRenderWindow(interactor.GetRenderWindow())
-    
-    #self.setLayout(self.layout)
+
+    # Disable interactor until data are present
+    self.viewer.GetRenderWindow().GetInteractor().Disable()
+
+    # Setup cursors and orientation of reslice image widget
+    rep = self.viewer.GetResliceCursorWidget().GetRepresentation()
+    rep.GetResliceCursorActor().GetCursorAlgorithm().SetReslicePlaneNormal(iDim)
+    self.viewer.SetSliceOrientation(iDim)
+    self.viewer.SetResliceModeToAxisAligned()
+  def SetResliceCursor(self, cursor):
+    self.viewer.SetResliceCursor(cursor)
+  def GetResliceCursor(self):
+    return self.viewer.GetResliceCursor()
   def start(self):
+    # Start interactor(s). Figure out to attach to Qt event-loop
     pass
-class Tester2D(QtWidgets.QFrame):
+
+class Tester2D(QFrame):
   def __init__(self, parent):
     super(Tester2D, self).__init__(parent)
     interactor = QVTKRenderWindowInteractor(self)
-    self.layout = QtWidgets.QHBoxLayout()
+    self.layout = QHBoxLayout()
     self.layout.addWidget(interactor)
     self.layout.setContentsMargins(0,0,0,0)
     self.setLayout(self.layout)
@@ -148,8 +232,8 @@ class TestMe(Viewer2D):
   pass
     
 if __name__ == '__main__':
-  if QtWidgets.QApplication.startingUp():
-    app = QtWidgets.QApplication(["FourPaneViewer"])
+  if QApplication.startingUp():
+    app = QApplication(["FourPaneViewer"])
   else:
     app = QCoreApplication.instance()
   main_window = FourPaneViewer()
