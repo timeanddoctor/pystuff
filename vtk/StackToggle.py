85e56dbe2b89ed737b26e8f5faa4d5ec58b333cb
Hansen <jem@bkmedical.com>
2021-01-31 19:31:54 +0100

    ..

--- vtk/StackToggle.py
+++ vtk/StackToggle.py
@@ -1,9 +1,22 @@
 import sys
 from PyQt5.QtWidgets import *
-
-class View2DStacked(QStackWidget):
-  def __init__(self, parent):
-    
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QPalette
+class View2DStacked(QStackedWidget):
+  def __init__(self, parent=None):
+    super(View2DStacked, self).__init__(parent)
+    for i in range(3):
+      widget = QWidget(self)
+      widget.setGeometry(0,0,400,300)
+      if 0:
+        pal = QPalette()
+        pal.setColor(QPalette.Background, {0:Qt.red, 1:Qt.green, 2:Qt.blue}[i])
+        widget.setAutoFillBackground(True)
+        widget.setPalette(pal)
+      else:
+        widget.setStyleSheet("background-color:" + {0:"red;", 1:"green;",
+                                                    2:"blue;"}[i])
+      self.addWidget(widget)
 
 if __name__ == "__main__":
   app = QApplication([])
@@ -14,7 +27,9 @@ if __name__ == "__main__":
 
   hbox = QHBoxLayout(w)
 
-  hbox.addWidget(btn1)
+  stack = View2DStacked()
+  
+  hbox.addWidget(stack)
 
   w.show()
 
