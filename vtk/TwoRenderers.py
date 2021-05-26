206c7b3e1297d8c7f0f3dd04336d41d5c5611b0d
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2021-05-26 18:50:04 +0200

    ..

--- vtk/TwoRenderers.py
+++ vtk/TwoRenderers.py
@@ -10,11 +10,12 @@ actor.SetMapper(mapper)
 
 renderer0 = vtk.vtkRenderer()
 renderer1 = vtk.vtkRenderer()
-renderer0.SetLayer(0)
+renderer0.SetLayer(0) # Below
 renderer1.SetLayer(1)
 render_window = vtk.vtkRenderWindow()
 render_window.SetNumberOfLayers(2)
 if 0:
+    # Clipping of camera are not computed correctly, because renderer1 has no actor
     render_window.AddRenderer(renderer0)
 
     # The following line triggers rendering problems
@@ -25,6 +26,18 @@ else:
 camera = vtk.vtkCamera()
 renderer0.SetActiveCamera(camera)
 renderer1.SetActiveCamera(camera)
+
+if 1:
+  actor0 = vtk.vtkActor()
+  sphere = vtk.vtkSphereSource()
+  sphere.SetPhiResolution(10)
+  sphere.SetThetaResolution(10)
+  sphere.Update()
+  
+  mapper0 = vtk.vtkPolyDataMapper()
+  mapper0.SetInputConnection(sphere.GetOutputPort())
+  actor0.SetMapper(mapper0)
+  renderer1.AddActor(actor0)
 
 renderer0.AddActor(actor)
 renderer0.ResetCamera()
